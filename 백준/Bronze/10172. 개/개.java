6fd4ba5cf86a48a4ca3549b78b62cefd5e06c399
20131827 <90309088+20131827@users.noreply.github.com>
2022-06-27 10:27:17 +0900

    [Bronze V] Title: 개, Time: 112 ms, Memory: 13944 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void main(String[] args){
+ 
+		StringBuilder sb = new StringBuilder();
+ 
+		sb.append("|\\_/|\n");
+		sb.append("|q p|   /}\n");
+		sb.append("( 0 )\"\"\"\\\n");
+		sb.append("|\"^\"`    |\n");    
+		sb.append("||_/=\\\\__|\n"); 
+        
+		System.out.println(sb);
+ 
+	}
+}
