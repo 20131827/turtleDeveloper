5b421b2abccd0d3b2c59a26720417356758e0e2a
20131827 <90309088+20131827@users.noreply.github.com>
2024-07-10 16:48:16 +0900

    [Silver I] Title: 현수막, Time: 224 ms, Memory: 23464 KB -BaekjoonHub

--- 백준/Silver/14716. 현수막/현수막.java
+++ 백준/Silver/14716. 현수막/현수막.java
@@ -1,52 +1,58 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-
-    static int[][] map;
-    static boolean[][] visited;
+    static int [][] map;
+    static boolean [][] visited;
     static int [] dx = {0, 0, -1, 1, 1, 1, -1, -1};
     static int [] dy = {1, -1, 0, 0, 1, -1, 1, -1};
-    static int result;
-    static int h,w;
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
+    static int y, x;
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        h = sc.nextInt(); // 세로길이
-        w = sc.nextInt(); // 가로길이
+        y = Integer.parseInt(st.nextToken());
+        x = Integer.parseInt(st.nextToken());
 
-        map = new int[h][w];
-        visited = new boolean[h][w];
-        result = 0;
+        map = new int [y][x];
+        visited = new boolean [y][x];
 
-        for(int i = 0 ; i < h ; i++){   // map 입력받기
-            for(int j = 0 ; j < w ; j++){
-                map[i][j] = sc.nextInt();
-            }
+        for(int i = 0 ; i < y ; i++){
+            map[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         }
 
-        for(int i = 0 ; i < h ; i++){  // dfs 실행
-            for(int j = 0 ; j < w ; j++){
-                if(!visited[i][j] && map[i][j] == 1){
+        int result = 0;
+        for(int i = 0 ; i < y ; i++){
+            for(int j = 0 ; j < x ; j++){
+                if(!visited[i][j] && map[i][j] == 1) {
                     dfs(i, j);
                     result++;
                 }
             }
         }
-
         System.out.println(result);
     }
+
     static void dfs(int i, int j){
-        if(!visited[i][j]){
-            visited[i][j] = true;
-            for(int a = 0 ; a < 8 ; a++) {
-                int y = i + dy[a];
-                int x = j + dx[a];
-                if (x >= 0 && y >= 0 && x < w && y < h && map[y][x] == 1) {
-                    dfs(y, x);
+        visited[i][j] = true;
+
+        Stack<int[]> stack = new Stack<>();
+        stack.push(new int[]{i, j});
+
+        while(!stack.isEmpty()){
+            int [] current = stack.pop();
+            int yy = current[0];
+            int xx= current[1];
+
+            for (int a = 0; a < 8; a++) {
+                int my = yy + dy[a];
+                int mx = xx + dx[a];
+
+                if( my >= 0 && mx >= 0 && my < y && mx < x && map[my][mx] == 1 && !visited[my][mx]){
+                    visited[my][mx] = true;
+                    stack.push(new int[]{my, mx});
                 }
             }
-        }else{
-            return;
         }
+
     }
 }
