e45c9942d366eb7aeb4e7ee85ccb6a4a2f8d768a
20131827 <90309088+20131827@users.noreply.github.com>
2025-06-17 11:50:39 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 100 ms, Memory: 14056 KB -BaekjoonHub

--- 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
@@ -7,9 +7,8 @@ public class Main {
         try{
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             StringTokenizer st;
-            boolean flag = true;
 
-            while(flag){
+            while(true){
                 st = new StringTokenizer(br.readLine());
                 int [] arr = new int [3];
                 arr [0] = Integer.parseInt(st.nextToken());
@@ -17,17 +16,16 @@ public class Main {
                 arr [2] = Integer.parseInt(st.nextToken()); // 가장 긴 변
                 Arrays.sort(arr);
 
-                if(arr[0] + arr[1] + arr[2] == 0){
-                    flag = false;
-                }
-                else if(arr[2] >= arr[0] + arr[1]){ // 가장긴 길이 보다 나머지 두 변의 합이 길지 않을때
+                if(arr[0] + arr[1] + arr[2] == 0) break;
+
+                if(arr[2] >= arr[0] + arr[1]){ // 가장긴 길이 보다 나머지 두 변의 합이 길지 않을때
                     System.out.println("Invalid");
                 }else if(arr[0] == arr[1] && arr[1] == arr[2]){ // 세 변의 길이가 모두 같은 경우
                     System.out.println("Equilateral");
                 }else if(arr[0] != arr[1] && arr[1] != arr[2]){ // 세 변의 길이가 모두 다른 경우
                     System.out.println("Scalene");
-                }else if(arr[0] == arr[1] || arr[1] == arr[2] || arr[0] == arr[2]) { //두 변의 길이만 같은 경우
-                    System.out.println("Isosceles ");
+                }else{ //두 변의 길이만 같은 경우
+                    System.out.println("Isosceles");
                 }
             }
 
