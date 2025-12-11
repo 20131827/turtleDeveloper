c98ac1131778c530440f1efcb75500b3f29bdc2e
20131827 <90309088+20131827@users.noreply.github.com>
2025-12-11 17:50:40 +0900

    [Silver I] Title: 태권왕, Time: 112 ms, Memory: 14848 KB -BaekjoonHub

--- 백준/Silver/14562. 태권왕/태권왕.java
+++ 백준/Silver/14562. 태권왕/태권왕.java
@@ -6,37 +6,33 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken()); // 테스트케이스의 수
+        int n = Integer.parseInt(st.nextToken());
+
         for(int i = 0 ; i < n ; i++){
-
             st = new StringTokenizer(br.readLine());
-            int S = Integer.parseInt(st.nextToken()); // 태균이 점수
-            int T = Integer.parseInt(st.nextToken()); // 상대방 점수
+            int S = Integer.parseInt(st.nextToken()); // 태균
+            int T = Integer.parseInt(st.nextToken()); // 상대
 
-            Queue<int[]> q = new LinkedList<>();
+            Queue <int[]> q = new LinkedList<>();
             q.offer(new int[]{S, T, 0});
 
-            int result = 0;
-            if(S != T) {
-                while (!q.isEmpty()) {
-                    int [] temp_arr = q.poll();
-                    int tempS = temp_arr[0];
-                    int tempT = temp_arr[1];
-                    int tempR = temp_arr[2];
-                    if(tempS == tempT){
-                        System.out.println(tempR);
-                        break;
-                    }
-                    if(tempS > tempT){
-                        continue;
-                    }
-                    q.offer(new int[]{tempS * 2, tempT + 3, tempR +1});
-                    q.offer(new int[]{tempS + 1, tempT, tempR +1});
-                }
-            }else{
-                System.out.println(result);
-            }
-        }
+            while(!q.isEmpty()){
+                int[] temp = q.poll();
+                int tempS = temp[0];
+                int tempT = temp[1];
+                int tempR = temp[2];
 
+                if(tempS == tempT){
+                    System.out.println(tempR);
+                    break;
+                }
+
+                if(tempS > tempT) continue;
+                q.offer(new int[]{tempS * 2, tempT + 3, tempR + 1});
+                q.offer(new int[]{tempS + 1, tempT, tempR + 1});
+
+            }
+
+        }
     }
 }
