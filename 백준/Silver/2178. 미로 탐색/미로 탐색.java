224a001add14707052a6fd8b0ff1fd3d13c372fe
20131827 <90309088+20131827@users.noreply.github.com>
2026-03-12 14:56:22 +0900

    [Silver I] Title: 미로 탐색, Time: 116 ms, Memory: 14752 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,55 +1,61 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
+
+    static boolean [][] visited;
     static int [] dx = {0, 0, -1, 1};
     static int [] dy = {-1, 1, 0, 0};
     static int [][] map;
-    static boolean [][] visited;
-    static int h = 0;
-    static int w = 0;
-    public static void main(String args[]) throws IOException{
-
+    static int n;
+    static int m;
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        h = Integer.parseInt(st.nextToken());
-        w = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken()); // 세로
+        m = Integer.parseInt(st.nextToken()); // 가로
 
-        map = new int[h][w];
-        visited = new boolean[h][w];
+        visited = new boolean[n][m];
+        map = new int [n][m];
 
-        for(int i = 0 ; i < h ; i++){
-            String input = br.readLine();
-            for(int j = 0 ; j < w ; j++) {
-                int temp = input.charAt(j) == '1' ? 1 : 0;
-                map[i][j] = temp;
+        for(int i = 0 ; i < n ; i++){
+            st = new StringTokenizer(br.readLine());
+            String temp = st.nextToken();
+            for(int j = 0 ; j < m ; j++){
+                map[i][j] = temp.charAt(j) - '0';
             }
         }
 
-        bfs(0, 0);
-        System.out.println(map[h-1][w-1]);
+        bfs();
+
+        System.out.println(map[n-1][m-1]);
+
     }
 
-    static void bfs(int a, int b){
-        Queue<int []> q = new LinkedList<>();
-        q.offer(new int[]{a,b});
+    public static void bfs(){
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{0, 0});
         visited[0][0] = true;
 
         while(!q.isEmpty()){
-            int[] currentMap = q.poll();
-            int y = currentMap[0];
-            int x = currentMap[1];
+            int[] cur = q.poll();
+            int y = cur[0];
+            int x = cur[1];
 
-            for(int i = 0 ; i < 4 ; i++){
-                int my = y + dy[i];
-                int mx = x + dx[i];
-                if (my >= 0 && mx >= 0 && my < h && mx < w && !visited[my][mx] && map[my][mx] == 1) {
-                    visited[my][mx] = true;
-                    map[my][mx] = map[y][x] +1;
-                    q.offer(new int[]{my, mx});
+            for(int i = 0 ; i < 4; i++){
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+
+                if(nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
+
+                if(!visited[ny][nx] && map[ny][nx] == 1){
+                    visited[ny][nx] = true;
+                    map[ny][nx] = map[y][x] + 1;
+                    q.offer(new int[]{ny, nx});
                 }
             }
-
         }
     }
+
 }
